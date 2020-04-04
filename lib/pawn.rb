e0e483a0ceb9b4fbad99ac7caf2619af4ad25693
novakoda <novakoda9@gmail.com>
2020-04-04 13:56:14 -0500

    fixed game breaking bug on right side of board

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -16,15 +16,19 @@ class Pawn
         @pos[1] == 1 ? moves = [[@pos[0],@pos[1]+2],[@pos[0],@pos[1]+1]] : moves = [[@pos[0],@pos[1]+1]]
         jumps = [[@pos[0]+1,@pos[1]+1], [@pos[0]-1,@pos[1]+1]]
         jumps.each do |jump|
-          slot = $board.at_pos(jump)
-          moves << jump if !slot.nil? && slot.symbol != "_" && slot.color == "white"
+          unless jump.include?(8) || jump.include?(-1)
+            slot = $board.at_pos(jump)
+            moves << jump if !slot.nil? && slot.symbol != "_" && slot.color == "white"
+          end
         end
       else
         @pos[1] == 6 ? moves = [[@pos[0],@pos[1]-2],[@pos[0],@pos[1]-1]] : moves = [[@pos[0],@pos[1]-1]]
         jumps = [[@pos[0]+1,@pos[1]-1], [@pos[0]-1,@pos[1]-1]]
         jumps.each do |jump|
-          slot = $board.at_pos(jump)
-          moves << jump if !slot.nil? && slot.symbol != "_" && slot.color == "black"
+          unless jump.include?(8) || jump.include?(-1)
+            slot = $board.at_pos(jump)
+            moves << jump if !slot.nil? && slot.symbol != "_" && slot.color == "black"
+          end
         end
       end
       moves
